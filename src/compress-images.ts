86429f41dc2734e61e1201ae4ddc3211cfbe7a01
Cosimo Matteini <dev.matteini@gmail.com>
2024-06-07 12:43:27 +0200

    session for compress image

--- src/compress-images.ts
+++ src/compress-images.ts
@@ -1,30 +1,83 @@
-import { copyFileSync, existsSync, mkdirSync, readdirSync, rmSync, statSync } from "node:fs"
+import {
+    copyFileSync,
+    existsSync,
+    mkdirSync,
+    readdirSync,
+    rmSync,
+    statSync,
+} from "node:fs"
 import * as path from "node:path"
 import sharp from "sharp"
 import { imageTypesRegex } from "./images"
+import { Effect } from "effect"
+import { FileSystem } from "@effect/platform"
 
 const WIDTH_THRESHOLD = 1500
 
-export const compressImages = async (sourceDir: string, outputDir: string) => {
-    if (!existsSync(sourceDir)) {
-        console.error(`\nSource directory ${sourceDir} does not exist\n`)
-        process.exit(1)
+import * as Data from "effect/Data"
+
+export class DirectoryNotExists extends Data.TaggedError("DirectoryNotExists")<{
+    sourceDir: string
+}> {
+    override toString(): string {
+        return `Source directory ${this.sourceDir} does not exist`
     }
+}
 
-    console.log(`\nReading images from ${sourceDir}\n`)
+export const compressImagesEffect = (sourceDir: string, outputDir: string) =>
+    Effect.gen(function* (_) {
+        const fs = yield* _(FileSystem.FileSystem)
 
-    const outputDirAbsolute = path.join(sourceDir, outputDir)
-    rmSync(outputDirAbsolute, { recursive: true, force: true })
-    mkdirSync(outputDirAbsolute, { recursive: true })
+        const sourceDirExists = yield* _(
+            fs.exists(sourceDir),
+            Effect.mapError((x) => {
+                return new Error(`Foo bar error`, { cause: x })
+            }),
+        )
+        if (!sourceDirExists) {
+            return yield* _(Effect.fail(new DirectoryNotExists({ sourceDir })))
+        }
 
-    const tasks = readdirSync(sourceDir)
-        // keep-line
-        .filter((file) => imageTypesRegex.test(file))
-        .map((file) => processOne(path.join(sourceDir, file), outputDirAbsolute))
-    const results = await Promise.all(tasks)
+        console.log(`\nReading images from ${sourceDir}\n`)
 
-    console.log(`\nProcessed ${results.length} images \n`)
-    console.log(`\nDONE\n`)
+        const outputDirAbsolute = path.join(sourceDir, outputDir)
+        yield* _(fs.remove(outputDirAbsolute, { recursive: true, force: true }))
+        yield* _(fs.makeDirectory(outputDirAbsolute, { recursive: true }))
+
+        const sourceDirFiles = yield* _(fs.readDirectory(sourceDir))
+        const tasks = sourceDirFiles
+            .filter((file) => imageTypesRegex.test(file))
+            .map((file) =>
+                Effect.promise(() =>
+                    processOne(path.join(sourceDir, file), outputDirAbsolute),
+                ),
+            )
+
+        const results = yield* _(Effect.all(tasks, { concurrency: "inherit" }))
+
+        console.log(`\nProcessed ${results.length} images \n`)
+        console.log(`\nDONE\n`)
+    })
+
+export const compressImages = async (sourceDir: string, outputDir: string) => {
+    // if (!existsSync(sourceDir)) {
+    //     console.error(`\nSource directory ${sourceDir} does not exist\n`)
+    //     process.exit(1)
+    // }
+    // console.log(`\nReading images from ${sourceDir}\n`)
+    // const outputDirAbsolute = path.join(sourceDir, outputDir)
+    // rmSync(outputDirAbsolute, { recursive: true, force: true })
+    // mkdirSync(outputDirAbsolute, { recursive: true })
+    // const tasks = readdirSync(sourceDir)
+    //     // keep-line
+    //     .filter((file) => imageTypesRegex.test(file))
+    //     .map((file) =>
+    //         processOne(path.join(sourceDir, file), outputDirAbsolute),
+    //     )
+    // const results = await Promise.all(tasks)
+    //
+    // console.log(`\nProcessed ${results.length} images \n`)
+    // console.log(`\nDONE\n`)
 }
 
 const processOne = async (inputFile: string, outputDir: string) => {
--- src/resize-images.ts
+++ src/resize-images.ts
@@ -1,4 +1,4 @@
-import { compressImages } from "./compress-images"
+import { compressImages, compressImagesEffect } from "./compress-images"
 import { reportProcessedImages } from "./report-processed-images"
 import * as path from "node:path"
 
@@ -10,7 +10,24 @@ const processedDirAbsolute = path.join(sourceDirAbsolute, compressOutputDir)
 const finalImageSrcBaseUrl = `/team-photos/${compressOutputDir}`
 const jsonOutputFile = "images.json"
 
+import * as Effect from "effect/Effect"
+import { NodeContext, NodeFileSystem } from "@effect/platform-node"
+
 export const resizeImages = async () => {
-    await compressImages(sourceDirAbsolute, compressOutputDir)
-    await reportProcessedImages(processedDirAbsolute, jsonOutputFile, finalImageSrcBaseUrl)
+    await Effect.runPromise(
+        compressImagesEffect(sourceDirAbsolute, compressOutputDir).pipe(
+            Effect.provide(NodeFileSystem.layer),
+            Effect.catchTag("DirectoryNotExists", (error) =>
+                Effect.dieMessage(
+                    `Error directory not exits ${error.sourceDir}`,
+                ),
+            ),
+            Effect.withConcurrency(10),
+        ),
+    )
+    await reportProcessedImages(
+        processedDirAbsolute,
+        jsonOutputFile,
+        finalImageSrcBaseUrl,
+    )
 }
